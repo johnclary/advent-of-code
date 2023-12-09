5020d8e9555514ea66d04b1ab5d195ea493954dd
John Clary <john.clary@austintexas.gov>
2023-12-08 21:23:39 -0500

    comment

--- 2023/6/part_2.js
+++ 2023/6/part_2.js
@@ -17,6 +17,14 @@ function main(rows) {
   let travelTime = 0;
   let isInWinZone = false;
 
+  /**
+   * This charge time vs travel time is a parabolic function.
+   * you could solve with with arithmetic if you paid attention
+   * in school. or you can use your debugger to figure out
+   * when the charge time value is within the part of the 
+   * parabola that cooresponds with winning this fictional
+   * race
+   */
   let increment = 1;
   while (chargeTime <= targetTime) {
     console.log(chargeTime, travelTime);
